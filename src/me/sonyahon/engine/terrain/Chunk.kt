769c3a4b5bcc281ee356dd24e9516cbe53755d89
sonyahon <volkov030@gmail.com>
2020-09-30 01:44:18 +0300

    move c# generator to kotlin

--- src/me/sonyahon/engine/terrain/Chunk.kt
+++ src/me/sonyahon/engine/terrain/Chunk.kt
@@ -22,40 +22,192 @@ class Chunk(coordinates: Vector2f) : Entity(null, null, Material(ShaderManager.i
     }
 
     private fun generateMesh(): StaticMeshData {
-        val mesh = Mesh()
 
         val heightMap = generateHeightMap()
 
         val vertices = MutableList<Vector3f>(0) { Vector3f() }
         val normals = MutableList<Vector3f>(0) { Vector3f() }
         val indices = MutableList(0) { 0 }
-        val colors = MutableList<Vector3f>(0) { Vector3f() }
+//        val colors = MutableList<Vector3f>(0) { Vector3f() }
 
-        var currentIndex = 0
+//        var currentIndex = 0
+//        for (y in IntRange(0, CHUNK_SIZE.toInt() - 1)) {
+//            for (x in IntRange(0, CHUNK_SIZE.toInt() - 1)) {
+//                vertices.add(Vector3f(x.toFloat(), heightMap[y][x], y.toFloat()))
+//                normals.add(Vector3f(0f, 1f, 0f))
+//                colors.add(Vector3f(.5f, .9f, 0f))
+//
+//                if (y != CHUNK_SIZE.toInt() - 1 && x != CHUNK_SIZE.toInt() - 1) {
+//
+//                    indices.add(currentIndex);
+//                    indices.add(currentIndex + CHUNK_SIZE.toInt() + 1);
+//                    indices.add(currentIndex + CHUNK_SIZE.toInt());
+//
+//                    indices.add(currentIndex);
+//                    indices.add(currentIndex + 1);
+//                    indices.add(currentIndex + CHUNK_SIZE.toInt() + 1);
+//
+//                }
+//
+//                currentIndex++;
+//            }
+//        }
+
+        var lastQuadStartingPoint: Int = 0;
+
         for (y in IntRange(0, CHUNK_SIZE.toInt() - 1)) {
             for (x in IntRange(0, CHUNK_SIZE.toInt() - 1)) {
-                vertices.add(Vector3f(x.toFloat(), heightMap[y][x], y.toFloat()))
+
+                val height = heightMap[y][x]
+                val centerPoint = Vector3f(x.toFloat(), height, y.toFloat())
+
+                vertices.add(centerPoint.get(Vector3f()).add(Vector3f(-0.5f, 0f, 0.5f)))
+                vertices.add(centerPoint.get(Vector3f()).add(Vector3f(0.5f, 0f, 0.5f)))
+                vertices.add(centerPoint.get(Vector3f()).add(Vector3f(-0.5f, 0f, -0.5f)))
+                vertices.add(centerPoint.get(Vector3f()).add(Vector3f(0.5f, 0f, -0.5f)))
+
                 normals.add(Vector3f(0f, 1f, 0f))
-                colors.add(Vector3f(.5f, .9f, 0f))
+                normals.add(Vector3f(0f, 1f, 0f))
+                normals.add(Vector3f(0f, 1f, 0f))
+                normals.add(Vector3f(0f, 1f, 0f))
 
-                if (y != CHUNK_SIZE.toInt() - 1 && x != CHUNK_SIZE.toInt() - 1) {
+                indices.add(lastQuadStartingPoint)
+                indices.add(lastQuadStartingPoint + 1)
+                indices.add(lastQuadStartingPoint + 2)
 
-                    indices.add(currentIndex);
-                    indices.add(currentIndex + CHUNK_SIZE.toInt() + 1);
-                    indices.add(currentIndex + CHUNK_SIZE.toInt());
+                indices.add(lastQuadStartingPoint + 1)
+                indices.add(lastQuadStartingPoint + 3)
+                indices.add(lastQuadStartingPoint + 2)
 
-                    indices.add(currentIndex);
-                    indices.add(currentIndex + 1);
-                    indices.add(currentIndex + CHUNK_SIZE.toInt() + 1);
-
-                }
-
-                currentIndex++;
+                lastQuadStartingPoint += 4
             }
         }
 
+        var vertexOffset = vertices.size
+
+        for (y in IntRange(0, CHUNK_SIZE.toInt() - 1)) {
+            for (x in IntRange(0, CHUNK_SIZE.toInt() - 1)) {
+                var currentQuadHeight = heightMap[y][x]
+                var currentQuadFirstPoint = (x + y * CHUNK_SIZE) * 4
+
+                var currentQuad = Array<Vector3f>(4) { i -> vertices[(currentQuadFirstPoint + i).toInt()] }
+
+                if (y != CHUNK_SIZE.toInt() - 1 && x != CHUNK_SIZE.toInt() - 1) {
+                    var rightQuadHeight = heightMap[y][x + 1]
+                    var rightQuadFirstPoint = (x + 1 + y * CHUNK_SIZE) * 4
+                    var rightQuad = Array<Vector3f>(4) { i -> vertices[(rightQuadFirstPoint + i).toInt()] }
+
+                    if (rightQuadHeight != currentQuadHeight) {
+                        vertices.add(currentQuad[1])
+                        vertices.add(currentQuad[3])
+                        vertices.add(rightQuad[0])
+                        vertices.add(rightQuad[2])
+
+                        normals.add(Vector3f(1f, 0f, 0f))
+                        normals.add(Vector3f(1f, 0f, 0f))
+                        normals.add(Vector3f(1f, 0f, 0f))
+                        normals.add(Vector3f(1f, 0f, 0f))
+
+                        indices.add(vertexOffset)
+                        indices.add(vertexOffset + 2)
+                        indices.add(vertexOffset + 1)
+
+                        indices.add(vertexOffset + 1)
+                        indices.add(vertexOffset + 2)
+                        indices.add(vertexOffset + 3)
+
+                        vertexOffset += 4;
+                    }
+
+                    var bottomQuadHeight = heightMap[y + 1][x]
+                    var bottomQuadFirstPoint = (x + (y + 1) * CHUNK_SIZE) * 4;
+
+                    var bottomQuad = Array(4) { i -> vertices[(bottomQuadFirstPoint + i).toInt()] }
+
+                    if (bottomQuadHeight != currentQuadHeight) {
+                        vertices.add(currentQuad[0])
+                        vertices.add(currentQuad[1])
+                        vertices.add(bottomQuad[2])
+                        vertices.add(bottomQuad[3])
+
+                        normals.add(Vector3f(0f, 0f, 1f))
+                        normals.add(Vector3f(0f, 0f, 1f))
+                        normals.add(Vector3f(0f, 0f, 1f))
+                        normals.add(Vector3f(0f, 0f, 1f))
+
+                        indices.add(vertexOffset)
+                        indices.add(vertexOffset + 2)
+                        indices.add(vertexOffset + 1)
+
+                        indices.add(vertexOffset + 1)
+                        indices.add(vertexOffset + 2)
+                        indices.add(vertexOffset + 3)
+
+                        vertexOffset += 4
+                    }
+                } else if (y == CHUNK_SIZE.toInt() - 1 && x != CHUNK_SIZE.toInt() - 1) {
+                    var rightQuadHeight = heightMap[y][x + 1]
+                    var rightQuadFirstPoint = (x + 1 + y * CHUNK_SIZE) * 4;
+
+                    var rightQuad = Array(4) { i -> vertices[(rightQuadFirstPoint + i).toInt()] }
 
 
+                    if (rightQuadHeight != currentQuadHeight) {
+                        vertices.add(currentQuad[1])
+                        vertices.add(currentQuad[3])
+                        vertices.add(rightQuad[0])
+                        vertices.add(rightQuad[2])
+
+                        normals.add(Vector3f(1f, 0f, 0f))
+                        normals.add(Vector3f(1f, 0f, 0f))
+                        normals.add(Vector3f(1f, 0f, 0f))
+                        normals.add(Vector3f(1f, 0f, 0f))
+
+                        indices.add(vertexOffset)
+                        indices.add(vertexOffset + 2)
+                        indices.add(vertexOffset + 1)
+
+                        indices.add(vertexOffset + 1)
+                        indices.add(vertexOffset + 2)
+                        indices.add(vertexOffset + 3)
+
+                        vertexOffset += 4
+                    }
+                } else if (y != CHUNK_SIZE.toInt() - 1 && x == CHUNK_SIZE.toInt() - 1) {
+                    var bottomQuadHeight = heightMap[y + 1][x]
+                    var bottomQuadFirstPoint = (x + (y + 1) * CHUNK_SIZE) * 4;
+
+                    var bottomQuad = Array(4) { i -> vertices[(bottomQuadFirstPoint + i).toInt()] }
+
+                    if (bottomQuadHeight != currentQuadHeight) {
+                        vertices.add(currentQuad[0])
+                        vertices.add(currentQuad[1])
+                        vertices.add(bottomQuad[2])
+                        vertices.add(bottomQuad[3])
+
+                        normals.add(Vector3f(0f, 0f, 1f))
+                        normals.add(Vector3f(0f, 0f, 1f))
+                        normals.add(Vector3f(0f, 0f, 1f))
+                        normals.add(Vector3f(0f, 0f, 1f))
+
+                        indices.add(vertexOffset)
+                        indices.add(vertexOffset + 2)
+                        indices.add(vertexOffset + 1)
+
+                        indices.add(vertexOffset + 1)
+                        indices.add(vertexOffset + 2)
+                        indices.add(vertexOffset + 3)
+
+                        vertexOffset += 4
+                    }
+                }
+            }
+        }
+
+        val colors = List(vertices.size) { Vector3f(0.5f, 0.9f, 0f) }
+
+
+        val mesh = Mesh()
         mesh.setNormals(normals)
         mesh.setVertices(vertices)
         mesh.setIndices(indices)
